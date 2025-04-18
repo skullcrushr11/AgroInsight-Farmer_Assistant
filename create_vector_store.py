365baa4e7f035b93bf45dab2a7754ebe0a109590
Sambhav-Birudavolu <bosssambhav@gmail.com>
2025-04-19 00:00:48 +0530

    Update create_vector_store.py

--- create_vector_store.py
+++ create_vector_store.py
@@ -8,7 +8,7 @@ import json
 from pathlib import Path
 from pprint import pprint
 from huggingface_hub import login
-login(token = Huggingface_API_KEY)
+login(token = "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")
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 #This code is for loading crop descriptions, text files, json files
@@ -26,32 +26,37 @@ def text_content(filepath):
         return f.read()
 
 
-vector_store_map={}
 L=["wheat","toordal","rice","ragi","jowar","groundnut","cotton_hirustum","cotton_arboreum","corn","coffee_arabica","brinjal","bengalgram","bajra"] 
 #minilist=["wheat","toordal","rice"]
+total_documents=[]
 for i in L:
     print(i)
-    index = faiss.IndexFlatL2(len(hf.embed_query("Could you tell me the optimal conditions for growing rice?")))
-    vector_store= FAISS(
-        embedding_function=hf,
-        index=index,
-        docstore=InMemoryDocstore(),
-        index_to_docstore_id={},
-    )
-
     path1="Crop_database\\"+i+".txt"
     content=text_content(path1)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size = 65, chunk_overlap=0)
-    dlist=text_splitter.create_documents([content])
+    dlist = text_splitter.create_documents([content], metadatas=[{"crop": i}])
+
 
     path2="Crop_database\\"+i+".json"
     data = json.loads(Path(path2).read_text())
     content=json.dumps(data)
     document = Document(
         page_content=content,
+        metadata={"crop": i}
     )
     dlist.append(document)
-    uuids = [str(uuid4()) for _ in range(len(dlist))]
-    vector_store.add_documents(documents=dlist, ids=uuids)
-    vector_store_map[i]=vector_store
-    vector_store.save_local("vector_stores",f"{i}_faiss_index")
+    total_documents+=dlist
+
+
+
+index = faiss.IndexFlatL2(len(hf.embed_query("Could you tell me the optimal conditions for growing rice?")))
+unified_vector_store= FAISS(
+    embedding_function=hf,
+    index=index,
+    docstore=InMemoryDocstore(),
+    index_to_docstore_id={},
+)
+uuids = [str(uuid4()) for _ in range(len(total_documents))]
+unified_vector_store.add_documents(documents=total_documents, ids=uuids)
+
+unified_vector_store.save_local("vector_stores",f"unified_faiss_index")
